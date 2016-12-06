2f7cec8baa4cd883f59bb19b5a4f6469605b203f
Jameel Alsalam <Alsalam.Jameel@epa.gov>
2016-12-06 13:20:37 -0500

    initial setup.R

---
+++ R/setup.R
@@ -0,0 +1,18 @@
+# load packages
+
+library("tidyverse")
+
+# ggplot2
+# tibble
+# tidyr
+# readr
+# purrr
+# dplyr
+
+library("readxl")
+
+library("stringr")
+
+library("testthat")
+
+# library("bookdown")
